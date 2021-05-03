d3409788f3cc2b846fcc2b2300627f8233d1f8d1
Masayoshi Mizutani <mizutani@cookpad.com>
2021-05-03 21:34:09 +0900

    Add global secondary index to dynamodb named 'tertiary'

--- lib/octovy-stack.ts
+++ lib/octovy-stack.ts
@@ -56,6 +56,13 @@ export class OctovyStack extends cdk.Stack {
       indexName: "secondary",
       partitionKey: { name: "pk2", type: dynamodb.AttributeType.STRING },
       sortKey: { name: "sk2", type: dynamodb.AttributeType.STRING },
+      projectionType: dynamodb.ProjectionType.KEYS_ONLY,
+    });
+    this.metaTable.addGlobalSecondaryIndex({
+      indexName: "tertiary",
+      partitionKey: { name: "pk3", type: dynamodb.AttributeType.STRING },
+      sortKey: { name: "sk3", type: dynamodb.AttributeType.STRING },
+      projectionType: dynamodb.ProjectionType.KEYS_ONLY,
     });
 
     // SQS
