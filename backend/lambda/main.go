73d0b91a5d39f1360ef513fd2332f3830ac6b910
Masayoshi Mizutani <mizutani@cookpad.com>
2021-05-26 05:51:46 +0900

    Call cleanupTempDir at starting lambda

--- backend/lambda/main.go
+++ backend/lambda/main.go
@@ -2,16 +2,30 @@ package main
 
 import (
 	"os"
+	"path/filepath"
 
 	"github.com/m-mizutani/goerr"
 	"github.com/m-mizutani/golambda"
 	"github.com/m-mizutani/octovy/backend/pkg/controller"
 )
 
+func cleanupTempDir() {
+	files, err := filepath.Glob("/tmp/*.zip")
+	if err != nil {
+		panic(err)
+	}
+	for _, f := range files {
+		if err := os.Remove(f); err != nil {
+			panic(err)
+		}
+	}
+}
+
 func main() {
 	funcID := os.Getenv("LAMBDA_FUNC_ID")
 
 	golambda.Start(func(event golambda.Event) (interface{}, error) {
+		cleanupTempDir()
 		ctrl := controller.New()
 
 		switch funcID {
