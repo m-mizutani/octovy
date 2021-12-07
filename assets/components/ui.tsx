d0969b21f15d774b82ff0f893e5e9c39e073f4c2
Masayoshi Mizutani <mizutani@hey.com>
2021-12-07 23:24:36 +0900

    Update repository page UI (#34)

--- assets/components/ui.tsx
+++ assets/components/ui.tsx
@@ -1,6 +1,23 @@
 import Chip from "@mui/material/Chip";
 import * as model from "@/components/model";
 
+import ReportProblemIcon from "@mui/icons-material/ReportProblem";
+import AccessAlarmIcon from "@mui/icons-material/AccessAlarm";
+import BuildIcon from "@mui/icons-material/Build";
+import BeenhereIcon from "@mui/icons-material/Beenhere";
+import Tooltip from "@mui/material/Tooltip";
+
+import { makeStyles } from "@mui/styles";
+
+const useStyles = makeStyles((theme) => ({
+  vulnStatusIcon: {
+    marginTop: 4,
+    marginRight: 1,
+    marginLeft: 0,
+    marginBottom: 0,
+  },
+}));
+
 function labelColor(hex: string) {
   var r = parseInt(hex.substr(1, 2), 16);
   var g = parseInt(hex.substr(3, 2), 16);
@@ -24,4 +41,37 @@ export function RepoLabel(props: {
       }}
     />
   );
+}
+
+export function StatusIcon(props: {
+  status: model.vulnStatusType;
+  expiresAt?: number;
+}) {
+  const classes = useStyles();
+  switch (props.status) {
+    case "none":
+      return <ReportProblemIcon className={classes.vulnStatusIcon} />;
+    case "mitigated":
+      return <BuildIcon className={classes.vulnStatusIcon} />;
+    case "unaffected":
+      return <BeenhereIcon className={classes.vulnStatusIcon} />;
+    case "snoozed":
+      const now = new Date();
+      if (props.expiresAt) {
+        const diff = props.expiresAt - now.getTime() / 1000;
+        const expiresIn =
+          diff > 86400
+            ? Math.floor(diff / 86000) + " days left"
+            : Math.floor(diff / 3600) + " hours left";
+
+        return (
+          <Tooltip title={expiresIn}>
+            <AccessAlarmIcon className={classes.vulnStatusIcon} />
+          </Tooltip>
+        );
+      } else {
+        return <AccessAlarmIcon className={classes.vulnStatusIcon} />;
+      }
+  }
+  return;
 }
