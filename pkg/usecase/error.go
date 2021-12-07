3a024e6adf40366efac5420dc723c99f12251383
Masayoshi Mizutani <masayoshi.mizutani@dr-ubie.com>
2021-12-07 09:44:39 +0900

    Use zlog.Err

--- pkg/usecase/error.go
+++ pkg/usecase/error.go
@@ -1,11 +1,9 @@
 package usecase
 
 import (
-	"errors"
 	"time"
 
 	"github.com/getsentry/sentry-go"
-	"github.com/m-mizutani/goerr"
 	"github.com/m-mizutani/octovy/pkg/domain/model"
 	"github.com/m-mizutani/octovy/pkg/utils"
 )
@@ -19,20 +17,13 @@ func (x *Usecase) flushError() {
 func (x *Usecase) HandleError(ctx *model.Context, err error) {
 	// Logging
 	entry := ctx.Log()
-	var gerr *goerr.Error
-	if errors.As(err, &gerr) {
-		for key, value := range gerr.Values() {
-			entry = entry.With(key, value)
-		}
-		entry = entry.With("stacktrace", gerr.Stacks())
-	}
 
 	if x.config.SentryDSN != "" {
 		evID := sentry.CaptureException(err)
 		entry = entry.With("sentry.EventID", evID)
 	}
 
-	entry.Error(err.Error())
+	entry.Err(err).Error(err.Error())
 
 	if x.testErrorHandler != nil {
 		x.testErrorHandler(err)
